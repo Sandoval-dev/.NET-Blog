47cf327d99d91a1f3e8db568a497b3785375176e
Osman Erdem Kilic <osman_erdem1200@hotmail.com>
2025-03-10 18:48:41 +0300

    Add project files.

---
+++ BlogV1/Context/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogV1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogV1.Context
+{
+    public class BlogDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-ERDEM\\SQLEXPRESS; database=BlogV1; Integrated Security=True; TrustServerCertificate=True;");
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}

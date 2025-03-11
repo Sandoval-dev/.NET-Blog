5749cf3a01b6eafcbf873812ecce08f0c726614b
Osman Erdem Kilic <osman_erdem1200@hotmail.com>
2025-03-11 12:14:18 +0300

    Blog Details page created

--- BlogV1/Controllers/BlogsController.cs
+++ BlogV1/Controllers/BlogsController.cs
@@ -18,5 +18,11 @@ namespace BlogV1.Controllers
             var blogs=_context.Blogs.Where(x=>x.Status==1).ToList();
             return View(blogs);
         }
+
+        public IActionResult Details(int id)
+        {
+            var blog =_context.Blogs.Where(x=>x.Id==id).FirstOrDefault();
+            return View(blog);
+        }
     }
 }

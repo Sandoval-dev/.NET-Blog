700b603f13bef12a3d6c35a0e53588b50c750494
Osman Erdem Kilic <osman_erdem1200@hotmail.com>
2025-03-11 16:09:10 +0300

    Admin Template and Blogs Control Page created and designed.

--- BlogV1/Program.cs
+++ BlogV1/Program.cs
@@ -24,6 +24,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Blogs}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
